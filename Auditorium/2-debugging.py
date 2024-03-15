fb7bc9e4d45680e0d195dafcc29eb46b35c6bfd6
NaufalKusumo <108918982+NaufalKusumo@users.noreply.github.com>
2024-03-15 17:39:40 +0700

    Create 2-debugging.py

---
+++ Auditorium/2-debugging.py
@@ -0,0 +1,13 @@
+#Task
+# Fix the code below 👇
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
+#Answer
+# Fix the code below 👇
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
