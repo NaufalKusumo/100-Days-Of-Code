3c5ab98553198ecc78e71aa718139fa81161192d
NaufalKusumo <108918982+NaufalKusumo@users.noreply.github.com>
2024-03-16 13:18:38 +0700

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,11 @@
+# Write your code below this line 👇
+print("Oh but you said that you are \"tired\", huh?")
+print("Oh but you said that you are 'tired', huh?\n")
 
+print('Hello ' + input("What is your name?\n") + '!\n')
+# input di eksekusi terlebih dahulu, setelah tahu value dari input, maka fungsi print akan di eksekusi
+# Jika bingung dapat mencoba debugging, dan next step secara perlahan
+# Thony python IDE bagus untuk mengetahui debugging
+
+print(len(input("Where are you from?\n")))
+# Len menghitung jumlah karakter dari inputan
