f6e9aa2415ff5096e8863a52183a4a0d3b6da3c5
NaufalKusumo <108918982+NaufalKusumo@users.noreply.github.com>
2024-03-16 13:00:48 +0700

    Create 4-variables.py

---
+++ Auditorium/4-variables.py
@@ -0,0 +1,13 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+
+# Write your code below this line 👇
+temp = a
+a = b
+b = temp
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
