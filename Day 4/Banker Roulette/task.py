33b73d33f0d1512105a3ae6990dfcdf9fba74587
Naufal N. Kusumo <nopalji375@gmail.com>
2025-03-09 20:42:12 +0700

    day 4 complete

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,6 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+randFriend = random.randint(0, len(friends) - 1)
+print(friends[randFriend])
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,15 @@
+import random
+
+
+def calculateFight(user, computer):
+    if user == computer:
+        print("It's a draw!")
+    elif (user - computer) % 3 == 1:
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +36,19 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+# List of choices
+tools = [rock, paper, scissors]
+
+print("Choose rock (0), paper (1), or scissors (2):")
+userChoice = int(input())
+
+if userChoice not in [0, 1, 2]:
+    print("Invalid choice! Please select 0, 1, or 2.")
+else:
+    print(f"You chose:\n{tools[userChoice]}")
+
+    compChoice = random.randint(0, 2)
+    print(f"Computer chose:\n{tools[compChoice]}")
+
+    calculateFight(userChoice, compChoice)
