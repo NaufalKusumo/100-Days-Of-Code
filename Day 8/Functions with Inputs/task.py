69655dc45109788aa16b44031116b06314b2c3dc
Naufal N. Kusumo <nopalji375@gmail.com>
2025-03-11 08:52:51 +0700

    day 8-1

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,6 @@
+def greet():
+    print("Hello")
+    print("Hello Me")
+    print("Hello How do you do?")
+
+greet()
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,10 @@
 # Functions with input
 
-def greet_with_name(name):
+def greet_with_name(name, location):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
 
+    print(f"What is it like in {location}?")
 
-greet_with_name("Jack Bauer")
+
+greet_with_name("Jack Bauer", "US")
